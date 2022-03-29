7ec497038f3588049243b82cfe03d57d95c3fbd7
mick-olka <mickolka64@gmail.com>
2022-03-29 10:32:09 +0300

    download_archives

--- src/COMPONENTS/ADMIN/AdminPageC/Settings.jsx
+++ src/COMPONENTS/ADMIN/AdminPageC/Settings.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ChangePW from "../AuthAdmin/ChangePW";
-import {makeBackup, restoreBackup} from "../../../REDUX/reducers/adminReducer";
+import {
+    makeBackup,
+    restoreBackup
+} from "../../../REDUX/reducers/adminReducer";
+import {apiURL} from "../../../API/api";
+const download_db_link = apiURL + 'download_archive_db';
+const download_photos_link = apiURL + 'download_archive_photos';
 
 function Settings ({changePW}) {
-
     return (
         <div>
             Settings
-            <ChangePW />
+            <ChangePW changePW={changePW} />
             <button onClick={makeBackup} >Make Backup</button>
             <button onClick={restoreBackup} >Restore Backup</button>
+            <button ><a href={download_db_link}> Download Database Archive</a></button>
+            <button ><a href={download_photos_link}> Download Photos Archive</a></button>
         </div>
     );
 }
